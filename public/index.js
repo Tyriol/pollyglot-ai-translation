1dd3c10be76910ec68412578a1a2c2f19ab6bf58
Ryan <ryanshaunsmith@gmail.com>
2024-10-23 18:40:32 +0100

    update fetch url

--- public/index.js
+++ public/index.js
@@ -10,7 +10,7 @@ async function submitTranslationData(e) {
   const formData = gatherFormData();
 
   try {
-    const response = await fetch("http://localhost:3000/api/translate", {
+    const response = await fetch("/api/translate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
