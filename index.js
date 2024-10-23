281f721c4f3c3b9004fe156a2dcd71ad3689652c
Ryan <ryanshaunsmith@gmail.com>
2024-10-23 13:54:35 +0100

    add openai to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import * as OpenAI from "openai";
 // take input values from the form
 // translation
 // language
@@ -6,20 +7,49 @@
 // render response message from open ai api
 // start over button to go back to input screen
 
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function main(text, language) {
+  messages = [
+    {
+      role: "system",
+      content:
+        "You are an expert tranlator. You specialise in translating from English to French, Spanish or Japanese",
+    },
+    {
+      role: "user",
+      content: `Please translate this text: ${text} into ${language}`,
+    },
+  ];
+  const chatCompletion = await client.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages,
+    warmth: 0.8,
+  });
+  return chatCompletion.choices[0].message.content;
+}
+
 const form = document.querySelector("form");
 const startOverButton = document.querySelector(".start-over");
 
 form.addEventListener("submit", submitTranslationData);
 startOverButton.addEventListener("click", startOver);
 
-function submitTranslationData(e) {
+async function submitTranslationData(e) {
   e.preventDefault();
   console.log("Form submitted");
   const formData = gatherFormData();
+  const translation = await main(
+    formData.textToTranslate,
+    formData.selectedLanguage
+  );
   document.querySelector(".translation-input").classList.add("hide");
   document.querySelector(".translation-output").classList.remove("hide");
   document.querySelector(".original-text-output").innerHTML =
-    formData.textToTranslate + formData.selectedLanguage;
+    formData.textToTranslate;
+  document.querySelector(".translated-text-output").innerHTML = translation;
 }
 
 function startOver() {
@@ -46,5 +76,4 @@ function gatherFormData() {
     textToTranslate,
     selectedLanguage,
   };
-  // const translationLanguage =
 }
